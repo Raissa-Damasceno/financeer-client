e2ea6dcfaebc138ef90597bd70d73c7899f1f08a
Raissa Damasceno <raissa.cod@gmail.com>
2021-12-16 14:08:55 +0000

    update remove

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5005/api';
+const url = 'http://localhost:5005';
 
 export const fetchStocks = () => axios.get(url);
-export const createStock = (newStock) => axios.post(`${url}/investments`, newStock);
-export const deleteStock = (investmentId) => axios.delete(`${url}/investment/${investmentId}`);
+export const createStock = (newStock) => axios.post(`${url}/api/investments`, newStock);
+export const deleteStock = (investmentId) => axios.delete(`${url}/api/investment/${investmentId}`);
